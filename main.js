34dfcb4fa2f0c573b7ee0fd92a699e6d900d23ad
mertllc <mert.laleci@ug.bilkent.edu.tr>
2022-07-27 08:50:24 +0300

    repo oluşturma
    ilk yükleme ile birlikte proje repo oluşumu

---
+++ main.js
@@ -0,0 +1,101 @@
+// Sayfa açıldığında fonksiyon çalışır
+window.onload = init;
+// Main fonksiyon tanımlaması
+function init() {
+    // Standart map layer olarak seçilir
+    const standard = new ol.layer.Tile({
+        source: new ol.source.OSM(),
+    });
+    // Parsellerin ekleneceği tabloların tanımlanması
+    var table;
+    table = document.getElementById("table");
+    // Vektör kaynağı tanımlanır
+    const source = new ol.source.Vector();
+    // Vektör değişkeni tanımlanır
+    const vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+            // fill: new ol.style.Fill({
+            //     color: 'rgba(255, 255, 255, 0.2',
+            // }),
+            stroke: new ol.style.Stroke({
+                color: '#ffcc33',
+                width: 2,
+            }),
+            image: new ol.style.Circle({
+                radius: 7,
+                fill: new ol.style.Fill({
+                    color: '#ffcc33',
+                }),
+            }),
+        }),
+    });
+    // Map tanımlanır
+    const map = new ol.Map({
+        layers: [standard, vector],
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2,
+            projection: 'EPSG:4326'
+        }),
+        target: 'js-map',
+    });
+    // Geometrik şekillerin yerlerinin değiştirilmesi için modify tanımlanır
+    const modify = new ol.interaction.Modify({ source: source });
+    // Modify mape interaction olarak eklenir
+    map.addInteraction(modify);
+    //Geometrik çizim ve çizimi yakalama draw snap özellikleri tanımlanır
+    let draw, snap;
+    //Geometrik şekil seçimini html ile bağlantı kurulması
+    const typeSelect = document.getElementById('type');
+    get_features();
+    //Draw ve Snap fonksiyon içine intereaction olarak eklenir
+    function addInteractions() {
+        draw = new ol.interaction.Draw({
+            source: source,
+            type: typeSelect.value,
+        });
+        map.addInteraction(draw);
+        snap = new ol.interaction.Snap({ source: source });
+        map.addInteraction(snap);
+        draw.on('drawend', function (evt) {
+            console.log(evt.feature);
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+            var button = document.getElementById("add_button");
+            button.onclick = function () {
+                var sehir = document.getElementById("sehir");
+                var ilce = document.getElementById("ilce");
+                var table = document.getElementById("table");
+                var row = table.insertRow(1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                cell1.innerHTML = "1";
+                cell2.innerHTML = sehir.value;
+                cell3.innerHTML = ilce.value;
+                modal.style.display = "none";
+            }
+        })
+    };
+    //Tıklamaya göre draw ve snapin kaldırılması
+    typeSelect.onchange = function () {
+        map.removeInteraction(draw);
+        map.removeInteraction(snap);
+        addInteractions();
+    };
+    addInteractions();
+}
+function get_features() {
+
+    $.ajax({
+        url: 'https://localhost:44381/api/parcel',
+        dataType: 'json',
+        type: 'get',
+        contentType: 'application/json',
+        data: { "data": "check" },
+        success: function (data) {
+            console.log(data);
+        }
+    });
+}
